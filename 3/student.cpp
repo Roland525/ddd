1c1c31fe024974261c9ba4374c334e3c412f327d
Roland <161613147+Roland525@users.noreply.github.com>
2025-01-10 09:46:37 +0000

    3

--- 3/student.cpp
+++ 3/student.cpp
@@ -1,48 +1,58 @@
 #include <string>
 #include <iostream>
 #include <cmath>
+#include<vector>
 using namespace std;
 
-class Student
-{
+class Student {
 private:
     int id;
 public:
     string name;
     string surname;
     string kurs;
-    
+
     Student(){};
-    Student(const string& nm, const string& snm, const string& krs) : name(nm), surname(snm), kurs(krs)
+    Student(const string& nm, const string& snm, const string& krs)
     {
         name = nm;
         surname = snm;
         kurs = krs;
         id = random();
-    }
-    int get_id(){
+        }
+
+    int get_id() const {
         return id;
     }
 
-void get_info(){
-    cout << name << ", " << surname << ", " << kurs << "id = " << get_id() << endl;
+    void get_info() const {
+        cout << "name: " << name
+             << "surname: " << surname
+             << "class: " << kurs
+             << "ID: " << id << endl;
+    }
 };
 
-};
-int main()
-{
-    Student a1;
-    Student a2;
-    Student a3;
-    Student a4;
-    Student a5;
-    cout << "Name: " << endl;
-    cin >> a1.name;
-    cout << "Surname: " << endl;
-    cin >> a1.surname;
-    cout << "Class: " << endl;
-    cin >> a1.kurs;
-    a1.get_info();
+int main() {
+    string name, surname, kurs;
+    vector<Student> student;
+    int num = 5;
+
+    for (int i = 0; i < num; i++) {
+        cout << "name: ";
+        cin >> name;
+        cout << "surname: ";
+        cin >> surname;
+        cout << "class: ";
+        cin >> kurs;
+        cout << endl;
+        student.push_back(Student(name,surname,kurs));
+    }
+
+    cout << "Studenti:" << endl;
+    for (int i = 0; i < num; i++) {
+        student[i].get_info();
+    }
 
     return 0;
 }
