23ff03b336254b988baa42f659d0c339052cb3d8
Roland <161613147+Roland525@users.noreply.github.com>
2025-01-17 08:30:33 +0000

    5

--- 5/media_server.cpp
+++ 5/media_server.cpp
@@ -1,4 +1,59 @@
 #include <iostream>
-#include<string>
+#include <string>
 using namespace std;
 
+class Media{
+    protected:
+    string title;
+    string name;
+    string surname;
+     int age;
+    int size;
+    string extension;
+
+    public:
+        Media(){};
+        Media(string nm, string snm, int ag, int sz, string ex, string ti): name(nm), surname(snm), age(ag), size(sz), extension(ex), title(ti){};
+};
+    //text
+    class Text : public Media{
+
+
+    }; 
+    class Note{
+
+        };
+    class Book{
+
+        };
+    
+    class Video : public Media{
+        protected:
+        int lenghth;
+        int resolution;
+
+        public:
+
+
+    };
+    class Movie{
+
+        };
+    class Audio : public Media{
+
+    };
+    class Music{
+
+        };
+    class Image : public Media{
+
+    };
+    class Meme{
+
+        };
+    class Photo{
+
+        };
+int main(){
+
+}
