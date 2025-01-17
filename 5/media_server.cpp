313dd17b5d94284bdbd0f4b45197cfac68264e02
Roland <161613147+Roland525@users.noreply.github.com>
2025-01-17 10:56:59 +0000

    5

--- 5/media_server.cpp
+++ 5/media_server.cpp
@@ -1,59 +1,123 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
-class Media{
-    protected:
+class Media {
+protected:
     string title;
     string name;
     string surname;
-     int age;
+    int age;
     int size;
     string extension;
 
-    public:
-        Media(){};
-        Media(string nm, string snm, int ag, int sz, string ex, string ti): name(nm), surname(snm), age(ag), size(sz), extension(ex), title(ti){};
+public:
+    Media() {}
+    Media(string nm, string snm, int ag, int sz, string ex, string ti)
+        : name(nm), surname(snm), age(ag), size(sz), extension(ex), title(ti) {}
+
+    void input() {
+        cout << "Enter title: ";
+        cin >> title;
+        cout << "Enter name: ";
+        cin >> name;
+        cout << "Enter surname: ";
+        cin >> surname;
+        cout << "Enter age: ";
+        cin >> age;
+        cout << "Enter size (MB): ";
+        cin >> size;
+        cout << "Enter extension: ";
+        cin >> extension;
+
+        display();
+    }
+
+    void display() {
+        cout << "Title: " << title << ", Name: " << name << ", Surname: " << surname
+             << ", Age: " << age << ", Size: " << size << " MB, Extension: " << extension << endl;
+    }
 };
-    //text
-    class Text : public Media{
 
+// Text class
+class Text : public Media {
+public:
+    Text() : Media() {}
+    Text(string nm, string snm, int ag, int sz, string ex, string ti)
+        : Media(nm, snm, ag, sz, ex, ti) {}
+};
 
-    }; 
-    class Note{
+// Video class
+class Video : public Media {
+protected:
+    int length;
+    int resolution;
 
-        };
-    class Book{
+public:
+    Video() : Media(), length(0), resolution(0) {}
+    Video(string nm, string snm, int ag, int sz, string ex, string ti, int len, int res)
+        : Media(nm, snm, ag, sz, ex, ti), length(len), resolution(res) {}
 
-        };
-    
-    class Video : public Media{
-        protected:
-        int lenghth;
-        int resolution;
+    void input() {
+        Media::input();
+        cout << "Enter length (minutes): ";
+        cin >> length;
+        cout << "Enter resolution (e.g., 1080): ";
+        cin >> resolution;
+    }
 
-        public:
+    void display() {
+        Media::display();
+        cout << "Length: " << length << " minutes, Resolution: " << resolution << "p" << endl;
+    }
+};
 
+// Photo class
+class Photo : public Media {
+public:
+    Photo() : Media() {}
+    Photo(string nm, string snm, int ag, int sz, string ex, string ti)
+        : Media(nm, snm, ag, sz, ex, ti) {}
+};
 
-    };
-    class Movie{
+int main() {
+    vector<Media*> mediaCollection;
+    int choice;
+    Media* media = nullptr;
 
-        };
-    class Audio : public Media{
+    while (true) {
+        cout << "Select media type to add:\n";
+        cout << "1. Text\n2. Video\n3. Photo\n4. Display all media\n5. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
 
-    };
-    class Music{
+        if (choice == 1) {
+            media = new Text();
+        } 
+        else if (choice == 2) {
+            media = new Video();
+        } 
+        else if (choice == 3) {
+            media = new Photo();
+        } 
+        else if (choice == 4) {
+            for (Media* m : mediaCollection) {
+                m->display();
+            }
+            continue;
+        } else if (choice == 5) {
+            break;
+        } else {
+            cout << "Invalid choice. Please try again.\n";
+            continue;
+        }
 
-        };
-    class Image : public Media{
+        if (media) {
+            media->input();
+            mediaCollection.push_back(media);
+        }
+    }
 
-    };
-    class Meme{
-
-        };
-    class Photo{
-
-        };
-int main(){
-
+    return 0;
 }
