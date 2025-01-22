55dbad3de84ff075bdda9be4487417758c19ccf1
Roland <161613147+Roland525@users.noreply.github.com>
2025-01-22 12:29:17 +0000

    6

--- 6/rpg_character.cpp
+++ 6/rpg_character.cpp
@@ -1,18 +1,60 @@
 #include <iostream>
 #include <string>
+#include <cmath>
+#include <random>
 using namespace std;
 
 class User {
+    
     public:
         string name;
         int hp;
 
 };
-class Weapon : public User{
+class Weapon{
     public:
+        string name;
         int damage;
+        int dps;
+        int accuracy;
+};
+class Onehand : public Weapon{
+
 };
 
+class Knife : public Onehand{
+
+    public:
+        Knife() {
+        damage = rndm(10, 30);
+        dps = 80;
+        accuracy = 70; 
+        }
+
+};
+
+class Axe : public Onehand{
+
+    public:
+        Axe() {
+            damage = rndm(50, 90);
+            dps = 25;
+            accuracy = 50;
+        }
+};
+
+class Twohand : public Weapon{
+
+};
+
+int rndm(int lower, int upper) {
+    // Генератор случайных чисел
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dist(lower, upper);
+
+    return dist(gen);
+}
 
 int main(){
     int answer = 0;
@@ -38,15 +80,20 @@ int main(){
     if (answer == 2){
         answer = 0;
         cout << "Choose 2 hand weapon for first hand: ";
+
         for(int i= 0; i < 0; i++){
             cout << "(" << i << ")" << ;
         };
+
         cout << "You answer: ";
         cin >> answer;
     }
 
 
-    cout << ""
+    cout << "Choose armor for first player"; 
+    answer = 0;
+    
+
     return 0;
 }
 
