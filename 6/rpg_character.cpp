42adf00ce79d39c2d78c49755606610849781efc
Roland <161613147+Roland525@users.noreply.github.com>
2025-01-22 17:19:02 +0000

    6

--- 6/rpg_character.cpp
+++ 6/rpg_character.cpp
@@ -2,13 +2,19 @@
 #include <string>
 #include <cmath>
 #include <random>
+#include <vector>
 using namespace std;
 
 class User {
     
     public:
         string name;
-        int hp;
+        int hp = 100;
+        vector<Weapon*> inventory;
+
+    void addweapon(Weapon* weapon){
+        inventory.push_back(weapon);
+    }
 
 };
 class Weapon{
@@ -47,6 +53,24 @@ class Twohand : public Weapon{
 
 };
 
+class Sword : public Weapon{
+    public:
+        Sword(){
+            damage = rndm(80, 120);
+            dps = 10;
+            accuracy = 30;
+        }
+};
+
+class Spear : public Weapon{
+    public:
+        Spear(){
+            damage = rndm(60, 90);
+            dps = 50;
+            accuracy = 20;
+        }
+};
+
 int rndm(int lower, int upper) {
     // Генератор случайных чисел
     random_device rd;
@@ -58,37 +82,49 @@ int rndm(int lower, int upper) {
 
 int main(){
     int answer = 0;
+    User player1;
     string name_for_first_player;
 
     cout << "Enter name for 1 player: ";
     cin >> name_for_first_player;
+    player1.name = name_for_first_player;
 
-
-    cout << "(1) one hand weapon or (2) two hand weapon:" << endl;
+    cout << "(1) one hand weapon or (2) two hand weapon: ";
     cin >> answer;
     if (answer == 1){
         answer = 0;
-        cout << "Choose 1 hand weapon for first hand: ";
-        for(int i= 0; i < 0; i++){
-            cout << "(" << i << ")" << 
-        };
+        cout << "Choose 1 hand weapon for first hand: " << endl;
+        cout << "(1) Knife " << "(2) Axe" << endl;
         cout << "You answer: ";
         cin >> answer;
+            if (answer == 1){
+                cout << "Knfie equped" << endl;
+                Knife* knife = new Knife();
+                player1.addweapon(knife);
+            }
+            if (answer == 2){
+                cout << "Axe equped" << endl;
+                Axe* axe = new Axe();
+                player1.addweapon(axe);
+            }
         }
-
-
     if (answer == 2){
         answer = 0;
-        cout << "Choose 2 hand weapon for first hand: ";
-
-        for(int i= 0; i < 0; i++){
-            cout << "(" << i << ")" << ;
-        };
-
+        cout << "Choose 2 hand weapon for first hand: " << endl;
+        cout << "(1) Sword " << "(2) Spear" << endl;
         cout << "You answer: ";
         cin >> answer;
-    }
-
+            if (answer == 1){
+                cout << "Sword equped" << endl;
+                Sword* sword = new Sword();
+                player1.addweapon(sword);
+            }
+            if (answer == 2){
+                cout << "Spear equped" << endl;
+                Spear* spear = new Spear();
+                player1.addweapon(spear);
+            }
+        }
 
     cout << "Choose armor for first player"; 
     answer = 0;
