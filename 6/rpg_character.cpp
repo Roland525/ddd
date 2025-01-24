0c3021ab34cc88666cff41b03c32318018d0737a
Roland <161613147+Roland525@users.noreply.github.com>
2025-01-24 13:20:18 +0000

    6

--- 6/rpg_character.cpp
+++ 6/rpg_character.cpp
@@ -15,26 +15,70 @@ int rndm(int lower, int upper) {
     return dist(gen);
 }
 
+class Armor{
+    public:
+        int armor_hp = 0;
+        int armor_dps = 0;
+
+};
+
+class Leather : public Armor{
+    public:
+        Leather(){
+            armor_hp += 50;
+            armor_dps = 0;
+        }
+};
+
+class Chainmail : public Armor{
+    public:
+        Chainmail(){
+            armor_hp += 100;
+            armor_dps = -30;
+
+        }
+};
+
+class Gold : public Armor{
+    public:
+        Gold(){
+            armor_hp += 250;
+            armor_dps = -40;
+        }
+};
+
+
+
 class Weapon{
     public:
         int damage;
         int dps;
         int accuracy;
+        
 };
 
-class User {
+
+
+class User{
     
     public:
         string name;
-        int dps_with_armor;
         int hp = 100;
         vector<Weapon*> inventory;
+        vector<Armor*> armors;
 
     void addweapon(Weapon* weapon){
         inventory.push_back(weapon);
     }
+    void addArmor(Armor* armor) {
+    armors.push_back(armor);
+    hp += armor->armor_hp;
+    }
+
 
 };
+
+
 
 class Onehand : public Weapon{
 
@@ -83,41 +127,15 @@ class Spear : public Weapon{
         }
 };
 
-class Armor : User{
-
-};
-
-class Leather : public Armor{
-    public:
-        Leather(){
-            hp += 50;
-            dps_with_armor = 0;
-        }
-};
-
-class Chainmail : public Armor{
-    public:
-        Armor(){
-            hp += 100;
-            dps_with_armor = -30;
-
-        }
-
-};
-
-class Gold : public Armor{
-    public:
-        Gold(){
-            hp += 250;
-            dps_with_armor = -40;
-        }
-};
-
 
 int main(){
     int answer = 0;
     User player1;
+    User player2;
     string name_for_first_player;
+    string name_for_second_player;
+
+//-----------------------------------------------
 
     cout << "Enter name for 1 player: ";
     cin >> name_for_first_player;
@@ -135,6 +153,7 @@ int main(){
                 cout << "Knfie equped" << endl;
                 Knife* knife = new Knife();
                 player1.addweapon(knife);
+                
             }
             else if (answer == 2){
                 cout << "Axe equped" << endl;
@@ -160,10 +179,103 @@ int main(){
             }
         }
 
+    answer = 0;
     cout << "Choose armor for first player" << endl;
+    cout << "(1) Leather (+50 hp, -0 dps) " << "(2) Chainmail(+100 hp, -30 dps) " << "(3) Gold (+250 hp, -40 dps)" << endl;
+    cout << "You answer: ";
+    cin >> answer;
+    if(answer == 1){
+        cout << "Leather armor equped!" << endl;
+        Leather* leather = new Leather();
+        player1.addArmor(leather);
+    }
+    else if(answer == 2){
+        cout << "Chainmail armor equped!" << endl;
+        Chainmail* chainmail = new Chainmail();
+        player1.addArmor(chainmail);
+    }
+    else if(answer == 3){
+        cout << "Gold armor equped!" << endl;
+        Gold* gold = new Gold();
+        player1.addArmor(gold);
+    }
+
+//-----------------------------------------------
+    answer = 0;
+    cout << "Enter name for 2 player: ";
+    cin >> name_for_second_player;
+    player2.name = name_for_second_player;
+
+    cout << "(1) one hand weapon or (2) two hand weapon: ";
+    cin >> answer;
+    if (answer == 1){
+        answer = 0;
+        cout << "Choose 1 hand weapon: " << endl;
+        cout << "(1) Knife " << "(2) Axe" << endl;
+        cout << "You answer: ";
+        cin >> answer;
+            if (answer == 1){
+                cout << "Knfie equped" << endl;
+                Knife* knife = new Knife();
+                player2.addweapon(knife);
+                
+            }
+            else if (answer == 2){
+                cout << "Axe equped" << endl;
+                Axe* axe = new Axe();
+                player2.addweapon(axe);
+            }
+        }
+    else if (answer == 2){
+        answer = 0;
+        cout << "Choose 2 hand weapon: " << endl;
+        cout << "(1) Sword " << "(2) Spear" << endl;
+        cout << "You answer: ";
+        cin >> answer;
+            if (answer == 1){
+                cout << "Sword equped" << endl;
+                Sword* sword = new Sword();
+                player2.addweapon(sword);
+            }
+            else if (answer == 2){
+                cout << "Spear equped" << endl;
+                Spear* spear = new Spear();
+                player2.addweapon(spear);
+            }
+        }
 
     answer = 0;
-    
+    cout << "Choose armor for first player" << endl;
+    cout << "(1) Leather (+50 hp, -0 dps) " << "(2) Chainmail(+100 hp, -30 dps) " << "(3) Gold (+250 hp, -40 dps)" << endl;
+    cout << "You answer: ";
+    cin >> answer;
+    if(answer == 1){
+        cout << "Leather armor equped!" << endl;
+        Leather* leather = new Leather();
+        player2.addArmor(leather);
+    }
+    else if(answer == 2){
+        cout << "Chainmail armor equped!" << endl;
+        Chainmail* chainmail = new Chainmail();
+        player2.addArmor(chainmail);
+    }
+    else if(answer == 3){
+        cout << "Gold armor equped!" << endl;
+        Gold* gold = new Gold();
+        player2.addArmor(gold);
+    }
+//-----------------------------------------------
+    while(player1.hp <= 0 || player2.hp <= 0){
+        if(player1.hp <= 0){
+            cout << player2.name << " Win!";
+            break;
+        }
+        if(player2.hp <= 0){
+            cout << player1.name << " Win!";
+            break;
+       }
+       
+    }
 
     return 0;
 }
